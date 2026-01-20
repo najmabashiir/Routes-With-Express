52eb8825ea0104e454ab24cc0d83a48c0ca2d795
najmabashiir <najaxbashiirali>
2026-01-20 08:09:11 +0300

    Multiple Route Parameters
    Signed-off-by: najmabashiir <najaxbashiirali>

--- app.js
+++ app.js
@@ -20,14 +20,26 @@
 // });
 
 // route parameters example
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/users/:userId', (req, res) =>{
+//   res.send(`User ID: ${req.params.userId}`);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// }); 
+
+// Multiple Route Parameters
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/users/:userId', (req, res) =>{
-  res.send(`User ID: ${req.params.userId}`);
-});
-
+app.get('/products/:catgeory/:productid',(req,res)=>{
+  res.send(`Category:${req.params.catgeory},Product Id:${req.params.productid}`);
+})
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-}); 
+});
