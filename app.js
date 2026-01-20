e18cb84f2c953155af41fb4c8e9a389c83c25c00
najmabashiir <najaxbashiirali>
2026-01-20 08:19:46 +0300

    Query Parameter
    Signed-off-by: najmabashiir <najaxbashiirali>

--- app.js
+++ app.js
@@ -33,13 +33,24 @@
 // }); 
 
 // Multiple Route Parameters
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/products/:catgeory/:productid',(req,res)=>{
+//   res.send(`Category:${req.params.catgeory},Product Id:${req.params.productid}`);
+// })
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
+// Query Parameters
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.get('/products/:catgeory/:productid',(req,res)=>{
-  res.send(`Category:${req.params.catgeory},Product Id:${req.params.productid}`);
-})
+app.get('/search',(req,res)=>{
+  res.send(`Search Query: ${req.query.q}`);
+});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
