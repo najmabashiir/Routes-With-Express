2f015513cb84419b92e36a5a5bd0d8261cde6541
najmabashiir <najaxbashiirali>
2026-01-20 08:00:35 +0300

    route parameters example
    Signed-off-by: najmabashiir <najaxbashiirali>

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 //basic routing example
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the Home Page!');
+// });
+
+// app.get('/about', (req, res) => {
+//   res.send('About Us Page');
+// });             
+
+// app.get('/contact', (req, res) => {
+//   res.send('contact us at ');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
+// route parameters example
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Home Page!');
-});
-
-app.get('/about', (req, res) => {
-  res.send('About Us Page');
-});             
-
-app.get('/contact', (req, res) => {
-  res.send('contact us at ');
+app.get('/users/:userId', (req, res) =>{
+  res.send(`User ID: ${req.params.userId}`);
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-});
+}); 
